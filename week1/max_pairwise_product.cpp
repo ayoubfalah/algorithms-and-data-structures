a65d2e3a8bd30060b953ad4d6eedf78a76b60160
Ayoub Falah <ayoub_falah@hotmail.de>
2017-04-23 23:38:52 +0200

    Using c++ buildin swap function

--- week1/max_pairwise_product.cpp
+++ week1/max_pairwise_product.cpp
@@ -4,6 +4,7 @@
 using std::vector;
 using std::cin;
 using std::cout;
+using std::swap;
 
 int FindMaxIndex(vector<int> numbers, int to){
     int n = numbers.size();
@@ -14,19 +15,13 @@ int FindMaxIndex(vector<int> numbers, int to){
     return maxIndex;
 }
 
-void swap(vector<int> &numbers, int i, int j){
-     int temp = numbers[i];
-     numbers[i] = numbers[j];
-     numbers[j] = temp;
-}
-
 long long MaxPairwiseProduct(vector<int> numbers)
 {
    int n = numbers.size();
    int maxIndex1 = FindMaxIndex(numbers, n);
-   swap(numbers, maxIndex1, n-1);
+   swap(numbers[maxIndex1], numbers[n-1]);
    int maxIndex2 = FindMaxIndex(numbers, n-1);
-   swap(numbers, maxIndex2, n-2);
+   swap(numbers[maxIndex2], numbers[n-2]);
    long long result = ((long long)numbers[n-1]) * numbers[n-2];
    return result;
 }
